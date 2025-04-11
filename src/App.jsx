a2379c245e94f7a0e09eb99be1e725ecb4bcda12
vicky5571 <vickygp5571@gmail.com>
2025-04-12 01:37:49 +0700

    Navbar Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar />
       <Hero />
       <About />
     </main>
